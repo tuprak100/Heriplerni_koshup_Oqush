49832d2546b7211dc21d4550b3e155819c04ea28
tuprak100 <57823962+tuprak100@users.noreply.github.com>
2024-12-23 19:15:17 -0600

    Update script.js
    minor update on the image url.

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ const cardNumberDisplay = document.getElementById('card-number');\
 let currentCard = 0;\
 \
 // ***REPLACE THIS WITH YOUR ACTUAL GITHUB RAW URL***\
-const imageBaseURL = "https://github.com/tuprak100/Heriplerni_koshup_Oqush/tree/main/images";\
+const imageBaseURL = "https://github.com/tuprak100/Heriplerni_koshup_Oqush/tree/main/images/";\
 \
 for (let i = 1; i <= 192; i++) \{\
     images.push(imageBaseURL + `img$\{i\}.jpg`); // Or .png\
